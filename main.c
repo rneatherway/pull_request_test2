4f2565b5ca13aa6fd1d10b9b2c9eb0791421b105
Marco Gario <marcogario@github.com>
2020-05-28 18:23:26 +0200

    Remove alert from main.c

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main(int argc, char **argv) {
-        char *copy2;
-        copy2 = (char *)malloc(strlen(argv[0]));
-        strcpy(copy2, argv[0]);
-        return copy2;
+        //char *copy2;
+        //copy2 = (char *)malloc(strlen(argv[0]));
+        //strcpy(copy2, argv[0]);
+        //return copy2;
+        return 0; // BUGFIX
 }
